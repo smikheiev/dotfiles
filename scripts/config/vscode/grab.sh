87216fa0256f8eed148845b8882825deb504d424
Serhii Mikheiev <mikh.s.o@gmail.com>
2020-03-22 10:06:06 +0100

    Add vscode keybindings

--- scripts/config/vscode/grab.sh
+++ scripts/config/vscode/grab.sh
@@ -4,4 +4,6 @@ source $DOTFILES_SCRIPTS/helpers/echo.sh
 
 echo_grab_config "Visual Studio Code"
 
-cp ~/Library/Application\ Support/Code/User/settings.json $DOTFILES_CONFIGS/vscode/settings.json
+DIR=~/Library/'Application Support'/Code/User
+cp "$DIR/settings.json" $DOTFILES_CONFIGS/vscode/settings.json
+cp "$DIR/keybindings.json" $DOTFILES_CONFIGS/vscode/keybindings.json
--- scripts/config/vscode/use.sh
+++ scripts/config/vscode/use.sh
@@ -4,5 +4,9 @@ source $DOTFILES_SCRIPTS/helpers/echo.sh
 
 echo_use_config "Visual Studio Code"
 
-mkdir -p ~/Library/Application\ Support/Code/User && cp $DOTFILES_CONFIGS/vscode/settings.json "$_"
+DIR=~/Library/'Application Support'/Code/User
+mkdir -p "$DIR"
+cp $DOTFILES_CONFIGS/vscode/settings.json "$DIR"
+cp $DOTFILES_CONFIGS/vscode/keybindings.json "$DIR"
+
 $DOTFILES_SCRIPTS/config/vscode/install_extensions.sh
